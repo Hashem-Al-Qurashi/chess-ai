6fc41e3330cb4f62bfa4c6a3f5d6e9f853dda127
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-04-16 19:22:14 +0300

    Extend famous game data: The Opera Game (1858)

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -8,4 +8,5 @@ export interface FamousGame {
 export const famousGames: FamousGame[] = [
   { year: 1851, white: "Anderssen", black: "Kieseritzky", name: "The Immortal Game" },
   { year: 1852, white: "Anderssen", black: "Dufresne", name: "The Evergreen Game" },
+  { year: 1858, white: "Morphy", black: "Duke of Brunswick", name: "The Opera Game" },
 ]
