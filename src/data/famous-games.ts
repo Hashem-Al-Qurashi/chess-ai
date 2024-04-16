d8f337ad26f2f727888d8c9065db6a80b1e2b4d2
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-04-16 09:45:10 +0300

    Catalog The Evergreen Game (1852) as famous game

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -7,4 +7,5 @@ export interface FamousGame {
 
 export const famousGames: FamousGame[] = [
   { year: 1851, white: "Anderssen", black: "Kieseritzky", name: "The Immortal Game" },
+  { year: 1852, white: "Anderssen", black: "Dufresne", name: "The Evergreen Game" },
 ]
