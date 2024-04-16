67a242435624d1c21da2f5ea587ed50b38a1a990
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-04-16 08:16:08 +0300

    Add new endgame position entry: King and Rook vs King

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -5,4 +5,5 @@ export interface Endgame {
 
 export const endgamePositions: Endgame[] = [
   { name: "King and Queen vs King", fen: "4k3/8/8/8/8/8/8/4K2Q w - - 0 1" },
+  { name: "King and Rook vs King", fen: "4k3/8/8/8/8/8/8/4K2R w - - 0 1" },
 ]
