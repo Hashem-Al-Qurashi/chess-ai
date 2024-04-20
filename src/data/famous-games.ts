7aeb3e13693933edbf2ecdd4f69d3a9ac7f148ef
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-04-20 14:11:26 +0300

    Append Kasparov's Scotch (1991) to famous game list

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -19,4 +19,5 @@ export const famousGames: FamousGame[] = [
   { year: 1970, white: "Spassky", black: "Fischer", name: "Game 6, World Championship" },
   { year: 1972, white: "Fischer", black: "Petrosian", name: "Fischer's Sicilian" },
   { year: 1985, white: "Kasparov", black: "Karpov", name: "Kasparov's 24th" },
+  { year: 1991, white: "Kasparov", black: "Anand", name: "Kasparov's Scotch" },
 ]
