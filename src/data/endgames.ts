16378be4bbc750a5cb1c6c158b6fcb363b6fef8c
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-01 22:35:34 +0300

    Document endgame position: King and Pawn vs King

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -37,4 +37,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King and Queen vs King", fen: "4k3/8/8/8/8/8/8/4K2Q w - - 0 1" },
   { name: "King and Rook vs King", fen: "4k3/8/8/8/8/8/8/4K2R w - - 0 1" },
   { name: "King and 2 Bishops vs King", fen: "4k3/8/8/8/8/8/8/2B1KB2 w - - 0 1" },
+  { name: "King and Pawn vs King", fen: "4k3/8/8/8/8/8/4P3/4K3 w - - 0 1" },
 ]
