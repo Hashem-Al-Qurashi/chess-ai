6f4485d6909fb8b404e5729c7c70e196f190d1ac
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-08 15:19:53 +0300

    Record The Opera Game (1858) in famous game data

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -44,4 +44,5 @@ export const famousGames: FamousGame[] = [
   { year: 2018, white: "Carlsen", black: "Caruana", name: "WC Tiebreak" },
   { year: 1851, white: "Anderssen", black: "Kieseritzky", name: "The Immortal Game" },
   { year: 1852, white: "Anderssen", black: "Dufresne", name: "The Evergreen Game" },
+  { year: 1858, white: "Morphy", black: "Duke of Brunswick", name: "The Opera Game" },
 ]
