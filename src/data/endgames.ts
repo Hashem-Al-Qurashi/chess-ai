3bc07b6b9f09f512f2fe6d7d0e4c637a6ae857d6
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-11 20:48:21 +0300

    Refine Italian defense strategy

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -637,4 +637,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
