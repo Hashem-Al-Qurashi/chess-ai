99ebca7f39c7cc30bf3536028df83d14247b1003
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-13 14:00:49 +0300

    Refine King's Gambit defense strategy

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -640,4 +640,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
