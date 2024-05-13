40555864d600ebc41a1373fa0075cbc491c33d08
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-13 21:46:53 +0300

    Document windmill tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -639,4 +639,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
