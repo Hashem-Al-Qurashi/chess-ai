71884ed4419359862dccecae2e1757750a14b15c
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-14 15:55:18 +0300

    Refine Slav defense strategy

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -643,4 +643,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
