e282ed140ec81f972ecba9a01265076f2722fb90
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-15 08:11:38 +0300

    Study Scandinavian middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -644,4 +644,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
