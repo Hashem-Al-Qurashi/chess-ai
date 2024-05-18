673467d80fcb9407b63814b98fd6f14e7dd78aeb
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-18 13:09:49 +0300

    Note deflection recognition pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -645,4 +645,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
