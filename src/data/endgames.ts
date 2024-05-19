12262b8a80e7579c4bcec465b0d8853da7406ec6
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-19 10:47:54 +0300

    Add double check puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -647,4 +647,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
