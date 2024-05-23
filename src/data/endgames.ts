88c8d992e9af132f55238cd27c8de56b57cc79cd
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-23 18:48:48 +0300

    Record x-ray combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -655,4 +655,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
