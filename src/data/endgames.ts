8b8eb620d08282c93cb1fd3a1e1695fd63437581
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-23 13:13:51 +0300

    Add Bishop activity evaluation

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -652,4 +652,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Knight vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
