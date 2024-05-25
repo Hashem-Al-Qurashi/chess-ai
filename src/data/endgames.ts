3433f07b7763d8451423c6123e7f944cdf006627
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-25 08:08:58 +0300

    Add deflection puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -660,4 +660,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
