e8a418e43ee846479debeeee345243ae5ec51d19
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-05-28 08:53:25 +0300

    Register Lucena Position in endgame position

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -83,4 +83,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King and Rook vs King", fen: "4k3/8/8/8/8/8/8/4K2R w - - 0 1" },
   { name: "King and 2 Bishops vs King", fen: "4k3/8/8/8/8/8/8/2B1KB2 w - - 0 1" },
   { name: "King and Pawn vs King", fen: "4k3/8/8/8/8/8/4P3/4K3 w - - 0 1" },
+  { name: "Lucena Position", fen: "1K1k4/1P6/8/8/8/8/r7/2R5 w - - 0 1" },
 ]
