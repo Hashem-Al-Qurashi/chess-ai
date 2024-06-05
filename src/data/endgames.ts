02240c05c331af3b33feb3b275863b1361495300
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-05 14:12:37 +0300

    Document pin tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -671,4 +671,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
