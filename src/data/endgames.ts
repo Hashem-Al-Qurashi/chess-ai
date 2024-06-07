b65827c1ed4417abf99cdcdc6138d40a5565bb5c
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-07 15:29:50 +0300

    Refine Dutch defense strategy

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -674,4 +674,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
