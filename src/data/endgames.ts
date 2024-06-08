05897d2e430d3d1d1b9c0bc0e1b51ec52597ca08
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-08 17:59:17 +0300

    Analyze Bishop coordination principle

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -675,4 +675,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Knight vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
