0f7f2fd32b4e6e7ae79bb7ca1512439eb9b54490
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-18 13:43:37 +0300

    Add skewer puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -684,4 +684,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
