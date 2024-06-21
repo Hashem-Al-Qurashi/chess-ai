56da5ff79ca477144c630f9e05bc7ea0e66ecbc1
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-21 16:57:36 +0300

    Note x-ray recognition pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -690,4 +690,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
