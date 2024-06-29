423c5b1ecad486cc7cc7b02266de5d2b1e74f80f
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-29 20:41:08 +0300

    Update Pawn movement logic notes

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -696,4 +696,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
