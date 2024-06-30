b38df3c0812b808cedcc4657c82c4f1e5c03df38
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-06-30 21:17:07 +0300

    Document decoy tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -697,4 +697,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
