1308e11d179f8d7b27727babc380301518c46e1e
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-07-04 21:10:19 +0300

    Add interference puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -699,4 +699,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
