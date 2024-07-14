2bf5ecd56c7bd9d559254442110fc1ed01c5f2e5
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-07-14 19:39:17 +0300

    Refine King's Indian defense strategy

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -707,4 +707,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
