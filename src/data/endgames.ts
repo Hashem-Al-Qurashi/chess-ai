f68320496cef532f9d451b88076109fca9969d70
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-07-19 08:30:53 +0300

    Note discovered attack recognition pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -720,4 +720,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
