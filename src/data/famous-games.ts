88d90648b80f764192c6f0abbb57cf963a361fbc
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-07-23 13:35:30 +0300

    Update famous game with The Immortal Game (1851)

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -168,4 +168,5 @@ export const famousGames: FamousGame[] = [
   { year: 2013, white: "Carlsen", black: "Anand", name: "Carlsen's WC Win" },
   { year: 2016, white: "Caruana", black: "Carlsen", name: "Caruana's Brilliance" },
   { year: 2018, white: "Carlsen", black: "Caruana", name: "WC Tiebreak" },
+  { year: 1851, white: "Anderssen", black: "Kieseritzky", name: "The Immortal Game" },
 ]
