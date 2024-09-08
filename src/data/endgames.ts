a3af33e2c1ded2826a64f5e526737b36522da289
Hashem Al-Qurashi <fahradnan@gmail.com>
2024-09-08 16:39:23 +0300

    Store Philidor Position as endgame position data

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -249,4 +249,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King and 2 Bishops vs King", fen: "4k3/8/8/8/8/8/8/2B1KB2 w - - 0 1" },
   { name: "King and Pawn vs King", fen: "4k3/8/8/8/8/8/4P3/4K3 w - - 0 1" },
   { name: "Lucena Position", fen: "1K1k4/1P6/8/8/8/8/r7/2R5 w - - 0 1" },
+  { name: "Philidor Position", fen: "4k3/8/4K3/4P3/8/8/8/r7 b - - 0 1" },
 ]
