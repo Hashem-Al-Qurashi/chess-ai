9ffd90c3e658d9744459dcefbd2c086572bf6b66
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-01 08:07:04 +0300

    Add interference puzzle example

--- src/data/famous-games.ts
+++ src/data/famous-games.ts
@@ -503,4 +503,5 @@ export const famousGames: FamousGame[] = [
   { year: 1919, white: "Study", black: "Analysis", name: "Refine Sicilian defense strategy" },
   { year: 1890, white: "Study", black: "Analysis", name: "Expand Queen's Gambit repertoire data" },
   { year: 1945, white: "Study", black: "Analysis", name: "Record decoy combination motif" },
+  { year: 1906, white: "Study", black: "Analysis", name: "Add interference puzzle example" },
 ]
