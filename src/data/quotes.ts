a4081293a15c28694ac3bd19acdcc668f22244f6
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-02 12:16:06 +0300

    data: expand ECO opening database

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -5,4 +5,5 @@ export interface ChessQuote {
 
 export const chessQuotes: ChessQuote[] = [
   { author: "Mikhail Botvinnik", text: "Chess is the art which expresses the science of logic." },
+  { author: "Emanuel Lasker", text: "When you see a good move, look for a better one." },
 ]
