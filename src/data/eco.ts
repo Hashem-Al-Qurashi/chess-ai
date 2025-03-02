d329edfa65034cca7feb6d8c6f02bea56e390398
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-02 19:27:37 +0300

    data: add chess quote by pattern

---
+++ src/data/eco.ts
@@ -0,0 +1,9 @@
+export interface EcoOpening {
+  eco: string
+  name: string
+  moves: string
+}
+
+export const ecoOpenings: EcoOpening[] = [
+  { eco: "E94", name: "King's Indian: Orthodox Variation", moves: "1.d4 Nf6 2.c4 g6 3.Nc3 Bg7 4.e4 d6 5.Nf3 O-O 6.Be2 e5 7.O-O" },
+]
