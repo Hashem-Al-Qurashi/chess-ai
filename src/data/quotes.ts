13a6f33cf3403599fc4205f3db80c7a15e93ead0
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-02 13:11:05 +0300

    data: include chess in opening catalog

---
+++ src/data/quotes.ts
@@ -0,0 +1,8 @@
+export interface ChessQuote {
+  author: string
+  text: string
+}
+
+export const chessQuotes: ChessQuote[] = [
+  { author: "Mikhail Botvinnik", text: "Chess is the art which expresses the science of logic." },
+]
