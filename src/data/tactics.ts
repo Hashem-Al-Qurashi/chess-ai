9b970a9b1da185945c6eb56aceffac6b2cafca25
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-02 15:51:37 +0300

    data: add grandmaster quote

---
+++ src/data/tactics.ts
@@ -0,0 +1,8 @@
+export interface TacticalPattern {
+  name: string
+  description: string
+}
+
+export const tacticalPatterns: TacticalPattern[] = [
+  { name: "Bishop Pair Advantage", description: "Using two bishops' long-range diagonal control in open positions" },
+]
