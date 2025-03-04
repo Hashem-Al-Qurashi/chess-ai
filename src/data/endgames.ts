c26db2a6a761d5b51eecc73e336b8220a783c0b6
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-04 09:59:22 +0300

    Study Reti middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -725,4 +725,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
