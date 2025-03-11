86a308d22dfa74483995bb75732e0235eeadc966
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-11 19:12:43 +0300

    feat: extend opening classification

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -35,4 +35,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B33", name: "Sicilian: Sveshnikov Variation", moves: "1.e4 c5 2.Nf3 Nc6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 e5" },
   { eco: "C62", name: "Ruy Lopez: Steinitz Defense", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 d6" },
   { eco: "A44", name: "Old Benoni: Czech Variation", moves: "1.d4 c5 2.d5 e5" },
+  { eco: "B92", name: "Sicilian: Najdorf, Opocensky Variation", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 a6 6.Be2" },
 ]
