50fe68f69bb773ba5f71435d32f30368cb487167
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-17 15:43:35 +0300

    Document interference tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -745,4 +745,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
