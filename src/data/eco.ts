e54e76d26abe6b134803a900ab4af091637facb3
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-20 14:17:13 +0300

    data: add English Opening: Symmetrical, Four Knights opening variation

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -54,4 +54,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "A46", name: "Queen's Pawn: Torre Attack", moves: "1.d4 Nf6 2.Nf3 e6 3.Bg5" },
   { eco: "C82", name: "Ruy Lopez: Open, Italian Variation", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 a6 4.Ba4 Nf6 5.O-O Nxe4 6.d4 b5 7.Bb3 d5 8.dxe5 Be6 9.c3" },
   { eco: "B63", name: "Sicilian: Richter-Rauzer, Classical", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 Nc6 6.Bg5 e6 7.Qd2" },
+  { eco: "A35", name: "English Opening: Symmetrical, Four Knights", moves: "1.c4 c5 2.Nc3 Nc6 3.Nf3" },
 ]
