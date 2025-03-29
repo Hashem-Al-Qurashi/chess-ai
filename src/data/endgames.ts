561193c89a35f975a632b5e784e664079ce0f68f
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-29 16:33:34 +0300

    Study King's Indian middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -762,4 +762,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
