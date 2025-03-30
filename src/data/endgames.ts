01b75238a4674450c0a06611c7e046f467eaaad8
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-03-30 19:26:39 +0300

    Document skewer tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -763,4 +763,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
