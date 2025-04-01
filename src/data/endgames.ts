3179fc379d49a8416170e8930114029950bd665d
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-04-01 08:39:00 +0300

    Record windmill combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -764,4 +764,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
