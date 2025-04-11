ff8445ceae7a46c947672adbf71629e0c3bed024
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-04-11 10:52:41 +0300

    Expand Alekhine repertoire data

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -776,4 +776,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
