2be77bcee42a195bacc793ba8712173b5f908c8f
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-04-12 17:55:32 +0300

    Update Bishop movement logic notes

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -780,4 +780,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
