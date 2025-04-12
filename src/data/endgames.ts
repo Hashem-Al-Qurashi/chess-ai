d688fd3455c366680e31a3aa58c4cb9ce4855f9c
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-04-12 12:22:42 +0300

    Analyze Queen coordination principle

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -778,4 +778,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
