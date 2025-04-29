99e6381b13ee72f3180ca6c8db3813c031dd414f
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-04-29 12:29:25 +0300

    content: document Benoni Defense variation

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -169,4 +169,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B32", name: "Sicilian: Löwenthal Variation", moves: "1.e4 c5 2.Nf3 Nc6 3.d4 cxd4 4.Nxd4 e5" },
   { eco: "A86", name: "Dutch Defense: Leningrad Variation", moves: "1.d4 f5 2.c4 Nf6 3.g3 g6" },
   { eco: "C63", name: "Ruy Lopez: Schliemann Defense", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 f5" },
+  { eco: "A56", name: "Benoni Defense", moves: "1.d4 Nf6 2.c4 c5" },
 ]
