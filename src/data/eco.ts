48e42f7f0ce62ea9da098eea15dc828ad573f6f6
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-05-06 20:44:46 +0300

    data: append tactical motif description

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -186,4 +186,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "C34", name: "King's Gambit Accepted: Fischer Defense", moves: "1.e4 e5 2.f4 exf4 3.Nf3 d6" },
   { eco: "D50", name: "Queen's Gambit Declined: Classical", moves: "1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Bg5" },
   { eco: "A54", name: "Old Indian: Ukrainian Variation", moves: "1.d4 Nf6 2.c4 d6 3.Nc3 e5 4.Nf3 Nbd7 5.e4" },
+  { eco: "D85", name: "Grünfeld: Exchange Variation", moves: "1.d4 Nf6 2.c4 g6 3.Nc3 d5 4.cxd5 Nxd5" },
 ]
