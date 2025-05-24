7015f6fe6da87639e566bb14edaa3d11e75b4c3a
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-05-24 12:01:32 +0300

    data: add chess tactic - pattern

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -230,4 +230,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "C72", name: "Ruy Lopez: Modern Steinitz, Main Line", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 a6 4.Ba4 d6 5.O-O" },
   { eco: "E42", name: "Nimzo-Indian: Rubinstein, Main Line", moves: "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.e3 c5 5.Ne2" },
   { eco: "D76", name: "Neo-Grünfeld: Simagin Variation", moves: "1.d4 Nf6 2.c4 g6 3.g3 d5 4.Bg2 Bg7 5.cxd5 Nxd5 6.Nf3 Nb6 7.Nc3 Nc6 8.e3 O-O 9.O-O Re8" },
+  { eco: "D56", name: "Queen's Gambit Declined: Lasker Defense", moves: "1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Bg5 Be7 5.e3 O-O 6.Nf3 h6 7.Bh4 Ne4" },
 ]
