a7d8704529ac0d06005b797261e7b722fa0a50e5
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-07-10 17:45:27 +0300

    data: add chess tactic - pattern

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -364,4 +364,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B71", name: "Sicilian: Dragon, Levenfish Variation", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 g6 6.f4" },
   { eco: "D34", name: "Queen's Gambit Declined: Tarrasch, Prague Variation", moves: "1.d4 d5 2.c4 e6 3.Nc3 c5 4.cxd5 exd5 5.Nf3 Nc6 6.g3 Nf6 7.Bg2 Be7" },
   { eco: "C40", name: "King's Knight Opening", moves: "1.e4 e5 2.Nf3" },
+  { eco: "D94", name: "Grünfeld: Smyslov Variation", moves: "1.d4 Nf6 2.c4 g6 3.Nc3 d5 4.Nf3 Bg7 5.e3" },
 ]
