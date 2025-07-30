35697f6172fe5ad3d0f50f6886c26c3ed203e615
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-07-30 19:59:55 +0300

    Add zugzwang puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -535,4 +535,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Knight vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
