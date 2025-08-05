5ec1b2c0608f7e05fde5aa4fd808dfffdd657105
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-05 15:36:16 +0300

    Expand Catalan repertoire data

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -542,4 +542,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
