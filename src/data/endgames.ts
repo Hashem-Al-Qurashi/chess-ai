bc990a6fab0c7d397872819d7a6b5e50a4e61f3a
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-12 19:33:30 +0300

    Add pin puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -544,4 +544,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
