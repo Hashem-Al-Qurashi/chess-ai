b751f2f547328c8b0fb9cf4bf90fb1b1b50ad94a
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-23 10:55:43 +0300

    Improve Pawn endgame technique

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -554,4 +554,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
