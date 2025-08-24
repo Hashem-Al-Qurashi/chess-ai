5e18df4337a2a89cbb1638d688eb4c0d7059bb18
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-24 17:26:17 +0300

    Expand Nimzo-Indian repertoire data

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -556,4 +556,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
