e344b72669082801c64a1b84c58bd40c7419a681
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-24 13:11:18 +0300

    Record discovered attack combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -557,4 +557,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
