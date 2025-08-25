fb3756af9822b992104b55cfc5e4d0d723d5bbf8
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-08-25 08:01:08 +0300

    Note fork recognition pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -559,4 +559,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
