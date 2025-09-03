58e515cf23361101d67194b13396bc19ad912984
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-09-03 23:02:40 +0300

    data: include Queen's Gambit Accepted: Normal in opening catalog

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -439,4 +439,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "A04", name: "Reti Opening", moves: "1.Nf3" },
   { eco: "E96", name: "King's Indian: Orthodox, 7...Nbd7 8.Re1", moves: "1.d4 Nf6 2.c4 g6 3.Nc3 Bg7 4.e4 d6 5.Nf3 O-O 6.Be2 e5 7.O-O Nbd7 8.Re1" },
   { eco: "B70", name: "Sicilian: Dragon Variation", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 g6" },
+  { eco: "D21", name: "Queen's Gambit Accepted: Normal", moves: "1.d4 d5 2.c4 dxc4 3.Nf3" },
 ]
