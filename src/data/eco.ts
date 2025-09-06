b05edee8420d804550f3f51e62903770a6ef061b
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-09-06 13:24:52 +0300

    data: catalog Ruy Lopez: Closed, Chigorin, Main Line opening line

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -448,4 +448,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "C19", name: "French Defense: Winawer, Positional Main Line", moves: "1.e4 e6 2.d4 d5 3.Nc3 Bb4 4.e5 c5 5.a3 Bxc3+ 6.bxc3 Ne7 7.Nf3" },
   { eco: "B75", name: "Sicilian: Dragon, Yugoslav Attack", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 g6 6.Be3 Bg7 7.f3" },
   { eco: "B15", name: "Caro-Kann: Main Line", moves: "1.e4 c6 2.d4 d5 3.Nc3" },
+  { eco: "C99", name: "Ruy Lopez: Closed, Chigorin, Main Line", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 a6 4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O 9.h3 Na5 10.Bc2 c5 11.d4 Qc7" },
 ]
