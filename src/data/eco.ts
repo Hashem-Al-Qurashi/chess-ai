6a43d01a38d64c957c3dad17c6288c0b70ed7111
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-09-29 15:02:26 +0300

    data: add grandmaster quote

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -496,4 +496,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "A05", name: "Reti Opening: King's Indian Attack", moves: "1.Nf3 Nf6" },
   { eco: "C12", name: "French Defense: McCutcheon Variation", moves: "1.e4 e6 2.d4 d5 3.Nc3 Nf6 4.Bg5 Bb4" },
   { eco: "E91", name: "King's Indian: Classical, 6.Be2", moves: "1.d4 Nf6 2.c4 g6 3.Nc3 Bg7 4.e4 d6 5.Nf3 O-O 6.Be2" },
+  { eco: "B98", name: "Sicilian: Najdorf, Classical", moves: "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 a6 6.Bg5 e6 7.f4 Be7" },
 ]
