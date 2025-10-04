232be4d526bdd3be41bbde27a811ad0a1212cc39
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-10-04 17:30:22 +0300

    Document double check tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -566,4 +566,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
