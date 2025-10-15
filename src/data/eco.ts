d854b51152a98c5534ad730b29ba2de213d40d8c
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-10-15 21:58:06 +0300

    data: add chess quote by pattern

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -530,4 +530,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "E32", name: "Nimzo-Indian: Classical Variation", moves: "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.Qc2" },
   { eco: "A52", name: "Budapest Gambit: Fajarowicz Variation", moves: "1.d4 Nf6 2.c4 e5 3.dxe5 Ne4" },
   { eco: "B30", name: "Sicilian: Rossolimo Variation", moves: "1.e4 c5 2.Nf3 Nc6 3.Bb5" },
+  { eco: "C11", name: "French Defense: Classical, Steinitz Variation", moves: "1.e4 e6 2.d4 d5 3.Nc3 Nf6" },
 ]
