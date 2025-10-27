6e11853fb07436eb483a2c06f63640bf4e6070ac
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-10-27 17:36:56 +0300

    Analyze Rook coordination principle

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -573,4 +573,5 @@ export const endgamePositions: Endgame[] = [
   { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
