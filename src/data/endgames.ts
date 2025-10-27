ff037fa9ec5110dc72954db98de55dc5a262dd11
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-10-27 09:02:37 +0300

    Study Caro-Kann middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -572,4 +572,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Knight vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
