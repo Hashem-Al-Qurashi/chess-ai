650730982506624628c25e95700f84259cfe00ed
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-10-30 14:12:28 +0300

    data: register A25 opening code

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -564,4 +564,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "E34", name: "Nimzo-Indian: Classical, Noa Variation", moves: "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.Qc2 d5" },
   { eco: "C10", name: "French Defense: Classical Variation", moves: "1.e4 e6 2.d4 d5 3.Nc3" },
   { eco: "A30", name: "English Opening: Symmetrical Variation", moves: "1.c4 c5" },
+  { eco: "A25", name: "English Opening: Closed Variation", moves: "1.c4 e5 2.Nc3 Nc6 3.g3" },
 ]
