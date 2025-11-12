7bbdb73238ce2b7e5697f7b5c2e68c1a74821ace
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-11-12 10:20:22 +0300

    Add overloading puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -578,4 +578,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
