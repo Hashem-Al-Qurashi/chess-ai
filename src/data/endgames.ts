a6063bcdeac42536c83a7a4844d9ee584dd77710
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-11-30 11:40:02 +0300

    Record decoy combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -590,4 +590,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Knight vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
