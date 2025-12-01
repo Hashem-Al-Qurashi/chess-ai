74347e3db4fe8fb24d8412e6a0612089394d1b94
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-12-01 19:14:06 +0300

    content: document Catalan: Closed Variation variation

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -622,4 +622,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "C24", name: "Bishop's Opening: Berlin Defense", moves: "1.e4 e5 2.Bc4 Nf6" },
   { eco: "E10", name: "Queen's Indian: Petrosian System", moves: "1.d4 Nf6 2.c4 e6 3.Nf3 b6" },
   { eco: "A99", name: "Dutch Defense: Ilyin-Zhenevsky, Modern Main Line", moves: "1.d4 f5 2.c4 Nf6 3.g3 e6 4.Bg2 Be7 5.Nf3 O-O 6.O-O d6 7.Nc3 Qe8 8.b3" },
+  { eco: "E01", name: "Catalan: Closed Variation", moves: "1.d4 Nf6 2.c4 e6 3.g3 d5 4.Bg2" },
 ]
