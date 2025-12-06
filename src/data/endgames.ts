305f02b10a0513a3fb222b0341024dc4cbefdffb
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-12-06 16:32:51 +0300

    Record interference combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -595,4 +595,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Knight vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
