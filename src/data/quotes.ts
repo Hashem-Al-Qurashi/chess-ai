478bf33385525dee46325a0894f833e1f56b8e7c
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-12-08 11:53:58 +0300

    data: add grandmaster quote

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -156,4 +156,5 @@ export const chessQuotes: ChessQuote[] = [
   { author: "José Raúl Capablanca", text: "You may learn much more from a game you lose than from a game you win." },
   { author: "Mikhail Botvinnik", text: "Chess is the art which expresses the science of logic." },
   { author: "Emanuel Lasker", text: "When you see a good move, look for a better one." },
+  { author: "Viktor Korchnoi", text: "No chess grandmaster is normal; they only differ in the extent of their madness." },
 ]
