03d520d29b6a0f629529e92e201bae0a8c825fe5
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-12-09 21:49:27 +0300

    Document zugzwang tactical pattern

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -598,4 +598,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
