f43ba2ec7ab19598247c2d48d9073e4b60bfb86c
Hashem Al-Qurashi <fahradnan@gmail.com>
2025-12-18 23:56:32 +0300

    Update Knight movement logic notes

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -603,4 +603,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
