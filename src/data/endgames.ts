029cd047c59882c9ac9aac5826626e5c95881bbf
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-01-01 21:45:35 +0300

    Study Catalan middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -605,4 +605,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Queen vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Bishop vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
