33ff841df2100ee1b45f4ec2151343b258003a33
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-01-02 21:31:51 +0300

    data: expand ECO opening database

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -678,4 +678,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B09", name: "Pirc Defense: Austrian Attack", moves: "1.e4 d6 2.d4 Nf6 3.Nc3 g6 4.f4" },
   { eco: "B11", name: "Caro-Kann: Two Knights Variation", moves: "1.e4 c6 2.Nc3 d5 3.Nf3" },
   { eco: "A90", name: "Dutch Defense: Classical Variation", moves: "1.d4 f5 2.c4 Nf6 3.g3 e6 4.Bg2" },
+  { eco: "B45", name: "Sicilian: Taimanov, Four Knights", moves: "1.e4 c5 2.Nf3 e6 3.d4 cxd4 4.Nxd4 Nc6 5.Nc3 Nf6" },
 ]
