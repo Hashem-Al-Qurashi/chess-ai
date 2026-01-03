74b9604b9f20e6ab174c9cec73321d7133a74f40
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-01-03 12:11:37 +0300

    data: add Queen's Indian: Averbakh Variation opening variation

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -685,4 +685,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B47", name: "Sicilian: Taimanov, Bastrikov Variation", moves: "1.e4 c5 2.Nf3 e6 3.d4 cxd4 4.Nxd4 Nc6 5.Nc3 Qc7" },
   { eco: "E15", name: "Queen's Indian: Fianchetto Variation", moves: "1.d4 Nf6 2.c4 e6 3.Nf3 b6 4.g3" },
   { eco: "D15", name: "Slav Defense: Main Line", moves: "1.d4 d5 2.c4 c6 3.Nf3 Nf6 4.Nc3" },
+  { eco: "E14", name: "Queen's Indian: Averbakh Variation", moves: "1.d4 Nf6 2.c4 e6 3.Nf3 b6 4.e3" },
 ]
