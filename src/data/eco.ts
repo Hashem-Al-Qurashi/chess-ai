f1c61827d83cc43b47f288da21da058a89ebad6e
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-01-12 14:21:43 +0300

    data: add ECO E51 - Nimzo-Indian: Main Line

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -710,4 +710,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B00", name: "King's Pawn: Nimzowitsch Defense", moves: "1.e4 Nc6" },
   { eco: "E26", name: "Nimzo-Indian: Sämisch, Main Line", moves: "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.a3 Bxc3+ 5.bxc3 c5 6.e3" },
   { eco: "D16", name: "Slav Defense: Bronstein Variation", moves: "1.d4 d5 2.c4 c6 3.Nf3 Nf6 4.Nc3 dxc4 5.a4 Na6" },
+  { eco: "E51", name: "Nimzo-Indian: Main Line", moves: "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.e3 O-O 5.Nf3 d5" },
 ]
