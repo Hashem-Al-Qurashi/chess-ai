905bb216706989794e4a6fdfcab16afdeb6aaa61
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-03 09:31:16 +0300

    Study Sicilian middlegame plans

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -617,4 +617,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Queen Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
