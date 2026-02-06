ae4dcd6e4398bd4f6b35206db1c532709a41c311
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-06 13:24:25 +0300

    Add Rook activity evaluation

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -623,4 +623,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Bishop vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Pawn vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Knight Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
