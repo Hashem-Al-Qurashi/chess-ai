5928586de42a361b52ac25b9f968550e4a39fd0e
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-16 17:58:41 +0300

    Add fork puzzle example

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -626,4 +626,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
