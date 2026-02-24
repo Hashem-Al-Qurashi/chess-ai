318dd0b3e00ce1b8b938506af98a5e9a863a36d4
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-24 16:17:08 +0300

    data: register C41 opening code

--- src/data/eco.ts
+++ src/data/eco.ts
@@ -780,4 +780,5 @@ export const ecoOpenings: EcoOpening[] = [
   { eco: "B29", name: "Sicilian: Nimzowitsch Variation", moves: "1.e4 c5 2.Nf3 Nf6" },
   { eco: "C61", name: "Ruy Lopez: Bird's Defense", moves: "1.e4 e5 2.Nf3 Nc6 3.Bb5 Nd4" },
   { eco: "B17", name: "Caro-Kann: Steinitz Variation", moves: "1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Nd7" },
+  { eco: "C41", name: "Philidor Defense", moves: "1.e4 e5 2.Nf3 d6" },
 ]
