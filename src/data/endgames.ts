abc520f2e15c6ce07603c538ee751a120ca8ca82
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-26 23:34:11 +0300

    Record overloading combination motif

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -634,4 +634,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Rook vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "King vs Pawn Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
