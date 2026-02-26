373c42f727167de4d8f5ce2eba9091e188a996e7
Hashem Al-Qurashi <fahradnan@gmail.com>
2026-02-26 20:15:20 +0300

    Expand Benoni repertoire data

--- src/data/endgames.ts
+++ src/data/endgames.ts
@@ -631,4 +631,5 @@ export const endgamePositions: Endgame[] = [
   { name: "Pawn vs King Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "King vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
   { name: "Queen vs Rook Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
+  { name: "Rook vs Bishop Study", fen: "8/8/8/4k3/8/4K3/8/8 w - - 0 1" },
 ]
